c10a0647ad1a60598ae6bfffa48109146cc5f66c
larry <xspanni@gmail.com>
2021-11-25 22:22:53 +0800

    添加c++生成model供qml使用的示例（未完成）

---
+++ examples/ModelView/videoListModel.h
@@ -0,0 +1,30 @@
+//
+// Created by Larry on 2021/11/25.
+//
+
+#ifndef QTEMPTY_VIDEOLISTMODEL_H
+#define QTEMPTY_VIDEOLISTMODEL_H
+
+#include <QAbstractListModel>
+
+class VideoListModelPrivate;
+class VideoListModel {
+ Q_OBJECT
+  Q_PROPERTY(QString source READ source WRITE setSource)
+ public:
+  VideoListModel(QObject *parent = 0);
+  ~VideoListModel();
+  int rowCount(const QModelIndex &parent) const;
+  QVariant data(const QModelIndex &index, int role) const;
+  QHash<int, QByteArray> roleNames() const;
+  QString source() const;
+  void setSource(const QString &filePath);
+  Q_INVOKABLE QString errorString() const;
+  Q_INVOKABLE bool hasError() const;
+  Q_INVOKABLE void reload();
+  Q_INVOKABLE void remove(int index);
+ private:
+  VideoListModelPrivate *m_dptr;
+};
+
+#endif //QTEMPTY_VIDEOLISTMODEL_H
